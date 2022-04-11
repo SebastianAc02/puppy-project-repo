44367457b0d765f09a8ff6ab1bbb9f700d6f755e
SebastianAc02 <91855259+SebastianAc02@users.noreply.github.com>
2022-04-11 16:42:08 -0700

    Update Lab10_part2.js

--- script/Lab10_part2.js
+++ script/Lab10_part2.js
@@ -4,6 +4,7 @@ var leftPosition = -200;
 
 
 var x = 50;
+var y = 0;
 
 function puppy(){
 
@@ -19,13 +20,19 @@ function runnpuppy(){
     
     if(puppyposition > 10){
         puppy.style.left = x+"px";
+        puppy.style.top = y+"px";
          x = x+ 10 ;
+         y =y +10;
+         while(y > window.innerHeight){
+             y =0;
+         }
          console.log(puppyposition);
 
     }
     else{
         x = 50;
     }
+    
     
   
 }
